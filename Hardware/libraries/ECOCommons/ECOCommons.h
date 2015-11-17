b1acd8df8bea412b40efcc7223144659ecb3fea9
emmaanuel <emmanuel@iMac-de-Emmanuel.local>
2015-11-17 22:34:28 +0100

    ajout de constantes

--- Hardware/libraries/ECOCommons/ECOCommons.h
+++ Hardware/libraries/ECOCommons/ECOCommons.h
@@ -1,1 +1,12 @@
 #define ENCRYPTKEY "mysecretpassword"
+#define NODE_BASE 1
+#define NODE_ETAGE 2
+#define NODE_SALON 3
+#define NODE_JARDIN 4
+#define NODE_THERMOSTAT 5
+#define NODE_EDF 6
+#define NODE_GRENIER 7
+
+#define RF433KEY {0xXX,0xXX,0xXX}
+#define RFM69Retry 3
+#define RFM69RetryTimeout 40
