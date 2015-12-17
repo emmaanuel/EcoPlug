da8f7c5e490784ab5c97b30f7c1478ae77b992a1
emmaanuel <emmanuel@iMac-de-Emmanuel.local>
2015-12-18 00:02:07 +0100

    ajout suffix

--- www/api/index.php
+++ www/api/index.php
@@ -42,7 +42,7 @@ $app->run();
 function getTempbyDateNode($date, $node) {
 	try {
 		$db = getDB();
-		$sql = "select DATE_FORMAT(date, '%y-%m-%d %H:%i') as d, node as n, round(temp,1) as t, round(rh,1) as h , light as l from domo_temp where date(date)=:date and node =:node order by date";
+		$sql = "select DATE_FORMAT(date, '%y-%m-%d %H:%i') as d, node as n, round(temp,1) as t, round(rh,1) as h , light as l from domo_temp" . getDBSuffix() . " where date(date)=:date and node =:node order by date";
 		$stmt = $db->prepare($sql);
 		$stmt->bindParam("date", $date);
 		$stmt->bindParam("node", $node);
@@ -59,7 +59,7 @@ function getTempbyDateNode($date, $node) {
 function getTempLastDay() {
 	try {
 		$db = getDB();
-		$sql = "select ROUND(UNIX_TIMESTAMP(date)/(15 * 60))*15*60 as d, node as n, round(temp,1) as t, round(rh,1) as h , light as l from domo_temp where UNIX_TIMESTAMP(date)>=(UNIX_TIMESTAMP(now())-(3600*24)) group by d,n order by date";
+		$sql = "select ROUND(UNIX_TIMESTAMP(date)/(15 * 60))*15*60 as d, node as n, round(temp,1) as t, round(rh,1) as h , light as l from domo_temp" . getDBSuffix() . " where UNIX_TIMESTAMP(date)>=(UNIX_TIMESTAMP(now())-(3600*24)) group by d,n order by date";
 		$stmt = $db->prepare($sql);
 		$stmt->bindParam("date", $date);
 		$stmt->bindParam("node", $node);
@@ -76,7 +76,7 @@ function getTempLastDay() {
 function getTempLastWeek() {
 	try {
 		$db = getDB();
-		$sql = "select ROUND(UNIX_TIMESTAMP(date)/(3600))*3600 as d, node as n, round(temp,1) as t, round(rh,1) as h , light as l from domo_temp where UNIX_TIMESTAMP(date)>=(UNIX_TIMESTAMP(now())-(3600*24*7)) group by d,n order by date";
+		$sql = "select ROUND(UNIX_TIMESTAMP(date)/(3600))*3600 as d, node as n, round(temp,1) as t, round(rh,1) as h , light as l from domo_temp" . getDBSuffix() . " where UNIX_TIMESTAMP(date)>=(UNIX_TIMESTAMP(now())-(3600*24*7)) group by d,n order by date";
 		$stmt = $db->prepare($sql);
 		$stmt->bindParam("date", $date);
 		$stmt->bindParam("node", $node);
@@ -93,7 +93,7 @@ function getTempLastWeek() {
 function getTempLastMonth() {
 	try {
 		$db = getDB();
-		$sql = "select ROUND(UNIX_TIMESTAMP(date)/(3600))*3600 as d, node as n, round(temp,1) as t, round(rh,1) as h , light as l from domo_temp where UNIX_TIMESTAMP(date)>=(UNIX_TIMESTAMP(now())-(3600*24*7*30)) group by d,n order by date";
+		$sql = "select ROUND(UNIX_TIMESTAMP(date)/(3600))*3600 as d, node as n, round(temp,1) as t, round(rh,1) as h , light as l from domo_temp" . getDBSuffix() . " where UNIX_TIMESTAMP(date)>=(UNIX_TIMESTAMP(now())-(3600*24*7*30)) group by d,n order by date";
 		$stmt = $db->prepare($sql);
 		$stmt->bindParam("date", $date);
 		$stmt->bindParam("node", $node);
@@ -110,7 +110,7 @@ function getTempLastMonth() {
 function getLastTemp() {
 	try {
 		$db = getDB();
-		$sql = "select d,n,t,h,l from (select DATE_FORMAT(date, '%y-%m-%d %H:%i') as d, node as n , round(temp,1) as t, round(rh,1) as h, light as l from domo_temp order by d desc) as table1 group by n";
+		$sql = "select d,n,t,h,l from (select DATE_FORMAT(date, '%y-%m-%d %H:%i') as d, node as n , round(temp,1) as t, round(rh,1) as h, light as l from domo_temp" . getDBSuffix() . " order by d desc) as table1 group by n";
 		$stmt = $db->prepare($sql);
 		$stmt->execute();
 		$temp = $stmt->fetchAll(PDO::FETCH_OBJ);
@@ -128,7 +128,7 @@ function insertTemp() {
 	$t = (($temp->t == "")?null:$temp->t);
 	$h = (($temp->h == "")?null:$temp->h);
 	$l = (($temp->l=="")?null:$temp->l);
-	$sql = "INSERT INTO domo_temp VALUES(NOW(),:node,:temp,:humi,:rx,:light)";
+	$sql = "INSERT INTO domo_temp" . getDBSuffix() . " VALUES(NOW(),:node,:temp,:humi,:rx,:light)";
 	try {
 		$db = getDB();
 		$stmt = $db->prepare($sql);
@@ -149,7 +149,7 @@ function insertTemp() {
 function getEdfbyDate($date) {
 	try {
 		$db = getDB();
-		$sql = "select DATE_FORMAT(date, '%y-%m-%d %H:%i') as d, power as r from domo_edf where date(date)=:date";
+		$sql = "select DATE_FORMAT(date, '%y-%m-%d %H:%i') as d, power as r from domo_edf" . getDBSuffix() . " where date(date)=:date";
 		$stmt = $db->prepare($sql);
 		$stmt->bindParam("date", $date);
 		$stmt->execute();
@@ -165,13 +165,13 @@ function getEdfbyDate($date) {
 function getLastEdf() {
 	try {
 		$db = getDB();
-		$sql = "Select truncate((max(hp)-min(hp))*0.152868/1000+(max(hc)-min(hc))*0.1105588/1000,2) as euros from domo_edf where date(date)=CURDATE()";
+		$sql = "Select truncate((max(hp)-min(hp))*0.152868/1000+(max(hc)-min(hc))*0.1105588/1000,2) as euros from domo_edf" . getDBSuffix() . " where date(date)=CURDATE()";
 		$stmt = $db->prepare($sql);
 		$stmt->execute();
 		$prix = $stmt->fetchAll(PDO::FETCH_OBJ);
 		$euros = $edf[0]->euros;
 		if ($euros == null) $euros=0;
-		$sql = "SELECT DATE_FORMAT(date, '%y-%m-%d %H:%i:%s') as d, conso as pw,truncate(hc/1000,0) as hc ,truncate(hp/1000,0) as hp, " . $euros . " as euros FROM domo_edf_live order by date desc limit 1";
+		$sql = "SELECT DATE_FORMAT(date, '%y-%m-%d %H:%i:%s') as d, conso as pw,truncate(hc/1000,0) as hc ,truncate(hp/1000,0) as hp, " . $euros . " as euros FROM domo_edf_live" . getDBSuffix() . " order by date desc limit 1";
 		$stmt = $db->prepare($sql);
 		$stmt->execute();
 		$edf = $stmt->fetchAll(PDO::FETCH_OBJ);
@@ -194,7 +194,7 @@ function getEdfLastWeek() {
 		$euros = $edf[0]->euros;
 		if ($euros == null) $euros=0;
 		*/
-		$sql = "SELECT round(UNIX_TIMESTAMP(date)/(3600))*3600 as d, max(power,0) as pw, min(truncate(hc/1000,0)) as hc , min(truncate(hp/1000,0)) as hp FROM domo_edf where UNIX_TIMESTAMP(date)>=(UNIX_TIMESTAMP(now())-(3600*24*7)) group by d order by date desc ";
+		$sql = "SELECT round(UNIX_TIMESTAMP(date)/(3600))*3600 as d, max(power,0) as pw, min(truncate(hc/1000,0)) as hc , min(truncate(hp/1000,0)) as hp FROM domo_edf" . getDBSuffix() . " where UNIX_TIMESTAMP(date)>=(UNIX_TIMESTAMP(now())-(3600*24*7)) group by d order by date desc ";
 		$stmt = $db->prepare($sql);
 		$stmt->execute();
 		$edf = $stmt->fetchAll(PDO::FETCH_OBJ);
@@ -209,7 +209,7 @@ function getEdfLastWeek() {
 function insertEdf() {
 	$request = \Slim\Slim::getInstance()->request();
 	$temp = json_decode($request->getBody());
-	$sql = "INSERT INTO domo_edf_live VALUES(NOW(),:power,:tf,:hc,:hp)";
+	$sql = "INSERT INTO domo_edf_live" . getDBSuffix() . " VALUES(NOW(),:power,:tf,:hc,:hp)";
 	try {
 		$db = getDB();
 		$stmt = $db->prepare($sql);
@@ -231,12 +231,12 @@ function insertMotion() {
 	
 	try {
 		$db = getDB();
-		$sql = "SELECT value from domo_params where name='motion'";
+		$sql = "SELECT value from domo_params" . getDBSuffix() . " where name='motion'";
 		$stmt = $db->prepare($sql);
 		$stmt->execute();
 		$result = $stmt->fetch(PDO::FETCH_ASSOC);
 		if ($result["value"] == "enabled") {
-			$sql = "INSERT INTO domo_motion VALUES(NOW(),:node,:rx)";
+			$sql = "INSERT INTO domo_motion" . getDBSuffix() . " VALUES(NOW(),:node,:rx)";
 			$stmt = $db->prepare($sql);
 			$stmt->bindParam("node", $motion->n);
 			$stmt->bindParam("rx", $motion->r);
@@ -245,7 +245,7 @@ function insertMotion() {
 			$headers .='Reply-To: ecordente@free.fr'."\n";
 			$headers .='Content-Type: text/plain; charset="iso-8859-1"'."\n";
 			$headers .='Content-Transfer-Encoding: 8bit';
-			mail('trigger@recipe.ifttt.com', 'Detection de Mouvement', 'Message contenu de l email', $headers); 
+			#mail('trigger@recipe.ifttt.com', 'Detection de Mouvement', 'Message contenu de l email', $headers); 
 		}
 		
 	} catch(PDOException $e) {
@@ -257,7 +257,7 @@ function insertMotion() {
 function getLastMotion() {
 	try {
 		$db = getDB();
-		$sql = "Select node as n, date as d from domo_motion ORDER BY date DESC LIMIT 5";
+		$sql = "Select node as n, date as d from domo_motion" . getDBSuffix() . " ORDER BY date DESC LIMIT 5";
 		$stmt = $db->prepare($sql);
 		$stmt->execute();
 		$motion = $stmt->fetchAll(PDO::FETCH_OBJ);
@@ -272,7 +272,7 @@ function getLastMotion() {
 function getParams($name) {
 	try {
 		$db = getDB();
-		$sql = "Select * from domo_params where name=:name";
+		$sql = "Select * from domo_params" . getDBSuffix() . " where name=:name";
 		$stmt = $db->prepare($sql);
 		$stmt->bindParam("name", $name);
 		$stmt->execute();
@@ -290,7 +290,7 @@ function setParams($name) {
 		$request = \Slim\Slim::getInstance()->request();
 		$param = json_decode($request->getBody());
 		$db = getDB();
-		$sql = "update domo_params set value=:value where name=:name";
+		$sql = "update domo_params" . getDBSuffix() . " set value=:value where name=:name";
 		$stmt = $db->prepare($sql);
 		$stmt->bindParam("name", $name);
 		$stmt->bindParam("value", $param->value);
@@ -305,7 +305,7 @@ function setParams($name) {
 function getAllParams() {
 	try {
 		$db = getDB();
-		$sql = "Select * from domo_params";
+		$sql = "Select * from domo_params" . getDBSuffix() . "";
 		$stmt = $db->prepare($sql);
 		$stmt->execute();
 		$param = $stmt->fetchAll(PDO::FETCH_OBJ);
@@ -320,7 +320,7 @@ function getAllParams() {
 function getNextAction() {
 	try {
 		$db = getDB();
-		$sql = "Select * from domo_actions where processed=0 ORDER BY date ASC LIMIT 1";
+		$sql = "Select * from domo_actions" . getDBSuffix() . " where processed=0 ORDER BY date ASC LIMIT 1";
 		$stmt = $db->prepare($sql);
 		$stmt->execute();
 		$action = $stmt->fetchAll(PDO::FETCH_OBJ);
@@ -335,7 +335,7 @@ function getNextAction() {
 function getLastAction() {
 	try {
 		$db = getDB();
-		$sql = "Select date, action, processed from domo_actions ORDER BY date DESC LIMIT 5";
+		$sql = "Select date, action, processed from domo_actions" . getDBSuffix() . " ORDER BY date DESC LIMIT 5";
 		$stmt = $db->prepare($sql);
 		$stmt->execute();
 		$action = $stmt->fetchAll(PDO::FETCH_OBJ);
@@ -352,7 +352,7 @@ function processAction() {
 		$request = \Slim\Slim::getInstance()->request();
 		$action = json_decode($request->getBody());
 		$db = getDB();
-		$sql = "update domo_actions set processed=TRUE where id=:id";
+		$sql = "update domo_actions" . getDBSuffix() . " set processed=TRUE where id=:id";
 		$stmt = $db->prepare($sql);
 		$stmt->bindParam("id", $action->id);
 		$stmt->execute();
@@ -366,7 +366,7 @@ function processAction() {
 function insertAction() {
 	$request = \Slim\Slim::getInstance()->request();
 	$action = json_decode($request->getBody());
-	$sql = "INSERT INTO domo_actions VALUES(null, NOW(),:action,0)";
+	$sql = "INSERT INTO domo_actions" . getDBSuffix() . " VALUES(null, NOW(),:action,0)";
 	try {
 		$db = getDB();
 		$stmt = $db->prepare($sql);
@@ -382,7 +382,7 @@ function insertAction() {
 function logAction() {
 	$request = \Slim\Slim::getInstance()->request();
 	$action = json_decode($request->getBody());
-	$sql = "INSERT INTO domo_actions VALUES(null, NOW(),:action,1)";
+	$sql = "INSERT INTO domo_actions" . getDBSuffix() . " VALUES(null, NOW(),:action,1)";
 	try {
 		$db = getDB();
 		$stmt = $db->prepare($sql);
@@ -398,7 +398,7 @@ function logAction() {
 function logHeater() {
 	$request = \Slim\Slim::getInstance()->request();
 	$event = json_decode($request->getBody());
-	$sql = "INSERT INTO domo_heater_evt VALUES(null, NOW(),:event)";
+	$sql = "INSERT INTO domo_heater_evt" . getDBSuffix() . " VALUES(null, NOW(),:event)";
 	try {
 		$db = getDB();
 		$stmt = $db->prepare($sql);
@@ -428,9 +428,9 @@ function getBadInfo() {
 
 function insertBadScore($id, $speed) {
 	if ($speed<125){
-		$sql = "update domo_bad domo_bad set score=score+:speed, speed=:speed where playerid=:playerid";
+		$sql = "update domo_bad" . getDBSuffix() . " set score=score+:speed, speed=:speed where playerid=:playerid";
 	} else {
-		$sql = "update domo_bad domo_bad set speed=:speed where playerid=:playerid";
+		$sql = "update domo_bad" . getDBSuffix() . " set speed=:speed where playerid=:playerid";
 	}
 	try {
 		$db = getDB();
@@ -439,7 +439,7 @@ function insertBadScore($id, $speed) {
 		$stmt->bindParam("playerid", $id );
 		$stmt->execute();
 		if ($stmt->affected_rows == 0){
-			$stmt = $db->prepare("insert into domo_bad values (:playerid,:speed,:speed)");
+			$stmt = $db->prepare("insert into domo_bad" . getDBSuffix() . " values (:playerid,:speed,:speed)");
 			$stmt->bindParam("playerid", $id );
 			$stmt->bindParam("speed", $speed );
 			$stmt->execute();
@@ -453,7 +453,7 @@ function insertBadScore($id, $speed) {
 }
 
 function resetBad() {
-	$sql = "truncate table domo_bad";
+	$sql = "truncate table domo_bad" . getDBSuffix() . "";
 	try {
 		$db = getDB();
 		$stmt = $db->prepare($sql);
